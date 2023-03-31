f3b3f10f88424a1b9abbb5f353e3f7256a69f2c6
cold-pumpkin <pch4623@gmail.com>
2023-03-31 20:54:38 +0900

    Hero

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -20,25 +20,28 @@ class Webtoon extends StatelessWidget {
           MaterialPageRoute(
             builder: (context) =>
                 DetailScreen(title: title, thumb: thumb, id: id),
-            fullscreenDialog: true, // 바닥에서 새로운 창이 올라오도록 (뒤로가기 대신 X)
+            // fullscreenDialog: true, // 바닥에서 새로운 창이 올라오도록 (뒤로가기 대신 X)
           ),
         );
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge, // BorderRadius 적용하기 위해
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  )
-                ]),
-            child: Image.network(thumb),
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge, // BorderRadius 적용하기 위해
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              child: Image.network(thumb),
+            ),
           ),
           const SizedBox(
             height: 10,
