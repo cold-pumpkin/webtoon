1e69bb8510f330272eb3347318251f562a2fc8a3
cold-pumpkin <pch4623@gmail.com>
2023-04-01 15:18:31 +0900

    Webtoon App - Url Launcher

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/model/webtoon_detail_model.dart';
 import 'package:webtoon/model/webtoon_episode_model.dart';
+import 'package:webtoon/widgets/episode_widget.dart';
 
 import '../services/api_service.dart';
 
@@ -107,39 +108,7 @@ class _DetailScreenState extends State<DetailScreen> {
                   return Column(
                     children: [
                       for (var episode in snapshot.data!)
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.green.shade400,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 5,
-                                offset: const Offset(5, 5),
-                                color: Colors.black.withOpacity(0.1),
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  ),
-                                ]),
-                          ),
-                        )
+                        Episode(episode: episode, webtoonId: widget.id)
                     ],
                   );
                 }
